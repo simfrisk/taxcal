67e8c0d913817045708cfa236bec96a5ec3fea39
simfrisk <162731912+simfrisk@users.noreply.github.com>
2024-11-14 20:13:40 +0100

    Update taxCal.js
    Added KR to resualt

--- Script/taxCal.js
+++ Script/taxCal.js
@@ -6,5 +6,5 @@ buttonCalculate.onclick = function () {
     var inputValue = parseFloat(inputUttag.value); // Convert input value to a number
     var resultValue = (inputValue / 0.4) - inputValue;
 
-    resualt.textContent = resultValue;
+    resualt.textContent = resultValue + " kr";
 }
